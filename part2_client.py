03b41a492498e994ceb672c746df9fa8d61940b8
Jose Pablo Palero <72049924+jppalero@users.noreply.github.com>
2022-12-01 12:14:27 -0800

    Updating part2

--- part2_client.py
+++ part2_client.py
@@ -3,27 +3,27 @@ import time # for RTT
 
 # IP and port number server
 PING_Params = ("127.0.0.1", 12000)
-MESSAGE = "ping"
 
 client = socket(AF_INET, SOCK_DGRAM)
 
 #for loop sending 10 UDP pings
-for i in range(0, 10):
+for i in range(0, 5):
+    message = f"PING {i} - hello world"
     try:
         #setting timeout to 1 so we throw an error is more than 1 sec
         client.settimeout(1) 
         #todo: start timer
         t0 = time.time()
         #send message to UDP server
-        client.sendto(MESSAGE.encode('utf_8'), PING_Params)
+        client.sendto(message.encode('utf_8'), PING_Params)
         #getting message if not more than 1 sec
         data , addr = client.recvfrom(1024)
-        #todo: stop timer and calculate ttl
+        #calculate rtt
         rtt = (time.time()) - t0
         #print response and ttl
         print(f"Response: {data.decode('utf_8')} RTT: {rtt}") 
     except Exception as e:
-        print(f"Request timed out: {e}")
+        print(f"Request timed out")
 
 #close client
 client.close()
--- part2_server.py
+++ part2_server.py
@@ -1,24 +1,31 @@
 # UDPPingerServer.py 
 # We will need the following module to generate randomized lost packets 
-import random 
+import random
+import time 
 from socket import * 
 
 # Create a UDP socket  
-# Notice the use of SOCK_DGRAM for UDP packets 
+# SOCK_DGRAM for UDP packets 
 serverSocket = socket(AF_INET, SOCK_DGRAM)
 
 # Assign IP address and port number to socket 
 serverSocket.bind(('127.0.0.1', 12000)) 
+print('Server is ready to receive...')
 
 while True: 
-    # Generate random number in the range of 0 to 10 
-    rand = random.randint(0, 10)   
     # Receive the client packet along with the address it is coming from 
     message, address = serverSocket.recvfrom(1024)
-    # Capitalize the message from the client 
-    message = message.upper() 
-    # If rand is less is than 4, we consider the packet lost and do not respond 
-    if rand < 4: 
+
+    # Rand between 5 and 50 ms for dealy
+    rand_delay = random.randint(5, 50)/1000 #rand delay in ms
+    time.sleep(rand_delay)
+
+    # Randomly drop packet with a 10% prob otherwise respond
+    rand = random.randint(0, 10) # Generate random number in the range of 0 to 10 
+    # If rand is equal to 1 --> mimics 10% prob
+    if rand == 1: 
         continue 
-    # Otherwise, the server responds   
+    # Otherwise, the server responds 
+    # Replace hello world --> ditto
+    print(message)
     serverSocket.sendto(message, address)
